cef7ed207e2526bd7590667c3532cdff4f707459
dixonjafet@gmail.com <dixonjafet@gmail.com>
2026-01-27 10:09:33 +0000

    first request pending

--- models/analytics/anlyt_esp_abs_prov.sql
+++ models/analytics/anlyt_esp_abs_prov.sql
@@ -4,8 +4,4 @@ WITH get_src_abs_prov AS (
     SELECT * FROM {{ ref('src_abs_prov') }}
 )
 
-SELECT
-  "año" AS "año",
-  provincia,
-  abstencionismo
-FROM get_src_abs_prov
+SELECT * FROM get_src_abs_prov
---
+++ models/staging/src_abs_edad.sql
@@ -0,0 +1,5 @@
+WITH raw_abstencionismo_edad AS (
+  SELECT * FROM ELECTIONS.RAW.RAW_ELECTORES_VOTOABS_EDAD
+)
+
+SELECT * FROM raw_abstencionismo_edad
