8d41a85665e81d85fb7844b768f68bdee98080c1
dixonjafet@gmail.com <dixonjafet@gmail.com>
2026-01-27 09:16:58 +0000

    new table

--- models/analytics/anlyt_esp_abs_prov.sql
+++ models/analytics/anlyt_esp_abs_prov.sql
@@ -1,11 +1,10 @@
-{% set warehouse_name = 'DATA_WH' %} USE WAREHOUSE {{ warehouse_name }};
 
-WITH src_abs_prov AS (
+
+WITH get_src_abs_prov AS (
     SELECT * FROM {{ ref('src_abs_prov') }}
 )
 
 SELECT
-    "año" AS "año",
     provincia,
     abstencionismo
-FROM src_abs_prov
+FROM get_src_abs_prov
