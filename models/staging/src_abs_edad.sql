890189d45756ec1d22b021c9581e90751acbac45
Jafet Dixon <dixonjafet@gmail.com>
2026-01-29 12:06:33 +0000

    all new analitycs

--- models/staging/src_abs_edad.sql
+++ models/staging/src_abs_edad.sql
@@ -2,4 +2,14 @@ WITH raw_abstencionismo_edad AS (
   SELECT * FROM ELECTIONS.RAW.RAW_ELECTORES_VOTOABS_EDAD
 )
 
-SELECT * FROM raw_abstencionismo_edad
+SELECT 
+    "año",
+    CASE 
+        WHEN edad = '18-30' THEN '18-29'
+        ELSE edad 
+    END as edad,
+    electores,
+    "participación",
+
+
+FROM raw_abstencionismo_edad
